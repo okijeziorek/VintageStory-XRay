d3c09af40c3bad04b113428c826bb63a1627ec08
okijeziorek <oki.jeziorek@gmail.com>
2026-09-15 16:04:11 +0200

    Add shared X-Ray runtime state

---
+++ src/VintageStoryXRay/XRayRuntime.cs
@@ -0,0 +1,8 @@
+namespace VintageStoryXRay;
+
+internal static class XRayRuntime
+{
+    public static XRayState? State { get; set; }
+
+    public static XRayConfig Config => State?.Config ?? XRayConfig.Default();
+}
