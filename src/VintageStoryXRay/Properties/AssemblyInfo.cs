2b82731c7bc82a6127fe1b3c364219c6b49dc397
okijeziorek <oki.jeziorek@gmail.com>
2026-09-15 16:05:51 +0200

    Add explicit development assembly metadata

---
+++ src/VintageStoryXRay/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Vintage Story X-Ray")]
+[assembly: AssemblyDescription("Client-side X-Ray rendering prototype for Vintage Story 1.22.3")]
+[assembly: AssemblyCompany("okijeziorek")]
